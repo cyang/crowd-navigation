d05910c68df7f1dbab7efea89ae5a4ccc9a8e1cb
Greg Olmschenk <golmschenk@gmail.com>
2014-02-03 17:21:32 -0500

    Add session id property

--- crowd-navigation/models.py
+++ crowd-navigation/models.py
@@ -10,5 +10,6 @@ class Crowdee(db.Model):
 class Source(db.Model):
     current_user = db.UserProperty()
     direction = db.StringProperty()
+    session_id = db.StringProperty()
     pub_token = db.StringProperty()
     sub_token = db.StringProperty()
