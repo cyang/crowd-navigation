3de0c38a70484d961a7256a3c8c5602c2b68d43f
Greg Olmschenk <golmschenk@gmail.com>
2014-02-19 17:28:59 -0500

    Move configure to separate line

--- crowd-navigation/js/app.js
+++ crowd-navigation/js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
-var app = angular.module('crowdnavigation', ['services']).config(function($locationProvider) {
+var app = angular.module('crowdnavigation', ['services'])
+
+app.config(function($locationProvider) {
     $locationProvider.html5Mode(true);
 });
