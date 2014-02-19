c3413a9c581d0eae02f6150bb4cc60fee50a16b5
Greg Olmschenk <golmschenk@gmail.com>
2014-02-19 13:20:00 -0500

    Fix bug of missing comma

--- crowd-navigation/js/services.js
+++ crowd-navigation/js/services.js
@@ -5,8 +5,27 @@ var services = angular.module('services', ['ngResource']);
 services.factory('Room', ['$resource',
     function($resource){
 		return $resource('room', {}, {
-	    	create: {method: 'POST'}
+	    	create: {method: 'POST'},
 			enter: {method: 'GET'}
 	    });
     }
+]);
+
+services.factory('Channel', [
+	function(){
+		return{
+			open: function(token){
+				var channel = new goog.appengine.Channel(token);
+		        var handler = {
+		            'onopen': onOpened,
+		            'onmessage': onMessage,
+		            'onerror': function() {},
+		            'onclose': function() {}
+		        };
+		        var socket = channel.open(handler);
+		        socket.onopen = onOpened;
+		        socket.onmessage = onMessage;
+			}
+		}
+	}
 ]);
