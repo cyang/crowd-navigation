ba6f9b8af5d04c5cfc846a3b7a76decd54aeae36
Greg Olmschenk <golmschenk@gmail.com>
2014-02-28 14:34:39 -0500

    Add ngClip

--- crowd-navigation/js/app.js
+++ crowd-navigation/js/app.js
@@ -1,7 +1,13 @@
 'use strict';
 
-var app = angular.module('crowdnavigation', ['services'])
+var app = angular.module('crowdnavigation', ['services', 'ngClipboard'])
 
-app.config(function($locationProvider) {
+app.config(function($locationProvider)
+{
     $locationProvider.html5Mode(true);
 });
+
+app.config(['ngClipProvider', function(ngClipProvider)
+{
+    ngClipProvider.setPath("/js/ZeroClipboard.swf");
+}]);
